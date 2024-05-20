bd83f93a6e525dd68e39f8548cb11dcb87633b2a
Gabriel <gvqsilva@gmail.com>
2024-05-19 21:04:29 -0300

    Criando a pagina Sobre

--- src/script/script.js
+++ src/script/script.js
@@ -2,3 +2,15 @@
 function trocar(cor){
     document.body.style.background =cor;
 }
+
+//Função da página contato
+function validateForm() {
+    let nome = document.getElementById("Nome").value;
+    let telefone = document.getElementById("Telefone").value;
+    let email = document.getElementById("Email").value;
+    if (nome == "" || telefone == "" || email == "") {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+    return true;
+}
